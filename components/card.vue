ec10f9165882112fdf9b56374aece4e8955634ef
Vilictus <imvilictus@gmail.com>
2023-11-12 02:58:11 +0000

    init

---
+++ components/card.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ description }}</p>
+</template>
+
+<style>
+h4 {
+  @apply font-inter text-gray-500 font-normal 
+}
+</style>
+
+<script setup>
+const Props = defineProps({
+  description: String,
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,42 @@
 <template>
   <section id="_appContent">
-    
-    Hola
+
+    <section id="_appAbout">
+      <h2>Positive Statistics Highlight <br> Success of Our Company</h2>
+      <Card :description="'Hola mundo'" />
+      <div>
+        <p>Strong sales, positive feedback, and sustainability practices <br> showcase our commitment
+          to excellence and growth</p>
+        <NuxtLink to='#'>More details</NuxtLink>
+      </div>
+
+    </section>
 
   </section>
 </template>
 
 <style>
 section#_appContent {
-  @apply bg-white text-black min-h-xl
+  @apply bg-white text-black min-h-xl py-60 font-italiana
 }
 
+section#_appAbout {
+  @apply max-w-5xl m-auto flex items-start justify-center text-3xl flex-col w-full
+}
+
+section#_appAbout h2 {
+  @apply m-0 p-0 leading-tight
+}
+
+section#_appAbout a {
+  @apply decoration-underline text-black
+}
+
+section#_appAbout div {
+  @apply flex justify-between items-center w-full
+}
+
+section#_appAbout p {
+  @apply font-inter text-lg
+}
 </style>
